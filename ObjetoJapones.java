8ff4418bc6f5871081f6eb7308fa971974766dad
danrodriguess <danillo_rodrigues@outlook.com>
2017-02-21 16:37:09 -0300

    Add files via upload

---
+++ ObjetoJapones.java
@@ -0,0 +1,26 @@
+public class ObjetoJapones {
+	
+	private String nomeObjeto;
+	private String corObjeto;
+	private String materialObjeto;
+	
+	public String getNomeobjeto(){
+		return nomeObjeto;
+	}
+	public void setNomeobjeto(String nomeObjeto){
+		this.nomeObjeto = nomeObjeto;
+	}
+	public String getCorobjeto(){
+		return corObjeto;
+	}
+	public void setCorobjeto(String corObjeto){
+		this.corObjeto = corObjeto;
+	}
+	public String getMaterialobjeto(){
+		return materialObjeto;
+	}
+	public void setMaterialobjeto(String materialObjeto){
+		this.materialObjeto = materialObjeto;
+	}
+	
+}
---
+++ ProdutoDao.java
@@ -0,0 +1,29 @@
+
+public class ProdutoDao {
+	
+	public static void main(String[] args){
+		
+		Produto produto = new Produto();
+		produto.setNomeProduto("Produtooo");
+		
+		System.out.print(produto.getNomeProduto());
+		
+	}
+	
+	public void cadastrar (Produto produto){
+		
+	}
+
+	public void atualizar(Produto produto){
+		
+	}
+
+	public void excluir (Produto produto){
+		
+	}
+
+	public Produto[] listar(){
+		return null;
+		
+	}
+}
